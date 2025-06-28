81ebf91f512a3187279d41376320c771b8d6c896
Natalia Taira <nataliagtaira@gmail.com>
2025-06-28 12:49:58 -0300

    feat(candidatura): adiciona validações e definição automática da data de aplicação
    - Define dataAplicacao como não nula e inicializa automaticamente com a data atual via @PrePersist;
    - Adiciona restrição de não nulo e limite de 20 caracteres ao campo status;

--- src/main/java/com/example/temosvagas/entities/Candidatura.java
+++ src/main/java/com/example/temosvagas/entities/Candidatura.java
@@ -11,7 +11,10 @@ public class Candidatura {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private LocalDate dataAplicacao;
+
+    @Column(nullable = false, length = 20)
     private String status;
 
     @ManyToOne
@@ -21,4 +24,10 @@ public class Candidatura {
     @ManyToOne
     @JoinColumn(name = "vaga_id")
     private Vaga vaga;
+
+    // Adiciona a data da criação da requisição como a data de aplicação
+    @PrePersist
+    public void prePersist() {
+        this.dataAplicacao = LocalDate.now();
+    }
 }
