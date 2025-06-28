5ea5b200e501f227002c2f0cdceb64932c6340e0
Natalia Taira <nataliagtaira@gmail.com>
2025-06-28 12:39:35 -0300

    refactor(empresa)
    Ajusta entidade para herdar de Usuario e incluir dados corporativos
    
    - Remove campos email e senha, agora herdados da classe Usuario;
    - Adiciona campos razaoSocial e cnpj para identificação da empresa;
    - Adiciona relacionamento com Filial: uma empresa pode ter várias filiais;

--- src/main/java/com/example/temosvagas/entities/Empresa.java
+++ src/main/java/com/example/temosvagas/entities/Empresa.java
@@ -3,20 +3,24 @@ package com.example.temosvagas.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Empresa {
+public class Empresa  extends Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
 
-    @Column(unique = true)
-    private String email;
+    private String razaoSocial;
 
-    private String senha;
+    private String cnpj;
+
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
+    private List<Filial> filiais;
 }
